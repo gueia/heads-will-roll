6dd09c6b257e5266bf22620a687b5c5b4eb269be
guiea <offbeatpersona@gmail.com>
2022-12-24 22:20:47 +0900

    v0.1.0

--- main.py
+++ main.py
@@ -396,24 +396,9 @@ class HeadtextChanger:
 
 
 def chromedriver_update():
-    chrome_latest_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
-    current_list = os.listdir(os.getcwd())
-    chrome_list = []
-    for i in current_list:
-        path = os.path.join(os.getcwd(), i)
-        if os.path.isdir(path):
-            if 'chromedriver.exe' in os.listdir(path):
-                chrome_list.append(i)
-    old_version = list(set(chrome_list) - {chrome_latest_ver})
-    for i in old_version:
-        path = os.path.join(os.getcwd(), i)
-        shutil.rmtree(path)
-    if chrome_latest_ver not in current_list:
-        print("Updating chromedriver...")
-        chromedriver_autoinstaller.install()
-        print("Done!")
-    else:
-        pass
+    print("Checking for ChromeDriver updates...")
+    chromedriver_autoinstaller.install()
+    print("Done!")
 
 
 if __name__ == '__main__':
